cb78be5ef8ce86b2cee017f32eabd56e16cc8987
John <johnfarris217@gmail.com>
2023-05-01 01:40:24 -0500

    commit tree 7c76d3e75547b95e9aaed4f1f1c207b2ff7e7be3

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+    if array.length == 1
+        array.join
+    elsif array.length == 2
+        array.join(' and ')
+    else
+        new_array = array.each_with_index.map do |word, index|
+            if index < array.length - 1
+                word
+            else
+                "and #{word}"
+            end
+        end
+        new_array.join(', ')
+    end
 end
